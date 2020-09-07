dd81d93976e7473a973b2ad05722b1e48feb8ef0
Spencer D <spencer.dedrick@gmail.com>
2020-09-07 03:01:10 -0500

    bug fix

---
+++ build/precache-manifest.5be55a4dedfcd4f4fc219ca81bce20d5.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afc5e31738c712017e1e71264a8ba246",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3bc02c5ef09560ff4b51",
+    "url": "/static/css/main.e131b6ce.chunk.css"
+  },
+  {
+    "revision": "e470db4d70b08a50937f",
+    "url": "/static/js/2.70fe00c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.70fe00c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bc02c5ef09560ff4b51",
+    "url": "/static/js/main.236ad5f1.chunk.js"
+  },
+  {
+    "revision": "b185c7ab2b0d26b39f5e",
+    "url": "/static/js/runtime-main.4a9da349.js"
+  },
+  {
+    "revision": "6293b7c54d1f417db994a9ed0a8fb405",
+    "url": "/static/media/Photo1.6293b7c5.jpeg"
+  },
+  {
+    "revision": "9a581390ad5f1ad1cdf6fa1bec7138d9",
+    "url": "/static/media/Photo10.9a581390.jpeg"
+  },
+  {
+    "revision": "b5b3cab0d62ef048e1b36bd9d93121a8",
+    "url": "/static/media/Photo11.b5b3cab0.jpeg"
+  },
+  {
+    "revision": "719d95f0df02c52103655586410a8ae8",
+    "url": "/static/media/Photo12.719d95f0.jpeg"
+  },
+  {
+    "revision": "2d6319a43ab4e447d91a2bbfb4965574",
+    "url": "/static/media/Photo13.2d6319a4.jpeg"
+  },
+  {
+    "revision": "0ff83b73e3c3a63fc9903a0abf75c1ce",
+    "url": "/static/media/Photo14.0ff83b73.jpeg"
+  },
+  {
+    "revision": "1ba234b17b1a96f6e818479e05b40883",
+    "url": "/static/media/Photo15.1ba234b1.jpeg"
+  },
+  {
+    "revision": "a768227f3f8f7bd021e0287f8ce8f99c",
+    "url": "/static/media/Photo16.a768227f.jpeg"
+  },
+  {
+    "revision": "3e9faf9160acb21f7e0130617b77a7b4",
+    "url": "/static/media/Photo17.3e9faf91.jpeg"
+  },
+  {
+    "revision": "ee5d7f733a58c0c772752ce3ac633e2d",
+    "url": "/static/media/Photo18.ee5d7f73.jpeg"
+  },
+  {
+    "revision": "becee8ee62a42d3be10f1665ce094ad6",
+    "url": "/static/media/Photo19.becee8ee.jpeg"
+  },
+  {
+    "revision": "4e1bb0aafa163c0b6708db9aac1d9480",
+    "url": "/static/media/Photo2.4e1bb0aa.jpeg"
+  },
+  {
+    "revision": "6ff65e158295b2a9de9204dc19fc78ac",
+    "url": "/static/media/Photo20.6ff65e15.jpeg"
+  },
+  {
+    "revision": "d0f33fb3ef4ee8977b31be88a9e1e05d",
+    "url": "/static/media/Photo3.d0f33fb3.jpeg"
+  },
+  {
+    "revision": "261f15b99b6e401818271cab1d2211d6",
+    "url": "/static/media/Photo4.261f15b9.jpeg"
+  },
+  {
+    "revision": "393cc3991d11103f885008d9b638c616",
+    "url": "/static/media/Photo5.393cc399.jpeg"
+  },
+  {
+    "revision": "96ea39b7b5244a0632549573b7e31b12",
+    "url": "/static/media/Photo6.96ea39b7.jpeg"
+  },
+  {
+    "revision": "cdaa93aeaf022d7b94ace48abd534ebc",
+    "url": "/static/media/Photo7.cdaa93ae.jpeg"
+  },
+  {
+    "revision": "ccd2d3ef8b7afa958ed4b8c439dee2f9",
+    "url": "/static/media/Photo8.ccd2d3ef.jpeg"
+  },
+  {
+    "revision": "5ad37c749394f46fcf1098fa150adb14",
+    "url": "/static/media/Photo9.5ad37c74.jpeg"
+  },
+  {
+    "revision": "d2b01f73582507b603f20b037ef793b8",
+    "url": "/static/media/VKLogoRed.d2b01f73.png"
+  },
+  {
+    "revision": "62ce131cc30e03c9d8d716f8f2db79b1",
+    "url": "/static/media/VKWhite.62ce131c.png"
+  },
+  {
+    "revision": "9f2d3c8381f427b2b92dc42d807bf8ad",
+    "url": "/static/media/VKphoto.9f2d3c83.jpg"
+  },
+  {
+    "revision": "e6672022c9241668c824a925e416a0b8",
+    "url": "/static/media/annie-spratt-bB0EsYazzXY-unsplash.e6672022.jpg"
+  },
+  {
+    "revision": "7de3136cd1a40cedfb47d73590b1cc4c",
+    "url": "/static/media/elaine-casap-qgHGDbbSNm8-unsplash.7de3136c.jpg"
+  }
+]);
